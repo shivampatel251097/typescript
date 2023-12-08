7a762bfdff030a10d879b3b3ea72394467351d1f
Shivam Patel <shivam.patel@tatatechnologies.com>
2023-12-08 16:15:53 +0530

    ts config and primitive types understanding

---
+++ fundamentals/02-Primitive-types.ts
@@ -0,0 +1,61 @@
+let number1 = 5;
+let number2 = 6;
+
+console.log("Sum of a + b = ", number1 + number2);
+
+const FirstName = "Shivam";
+const LastName = "Patel";
+
+console.log( `My name is: ${FirstName} ${LastName} ` );
+
+const isActive = true;
+
+const isEnabled = false;
+
+console.log("The isctive flag is ", isActive);
+console.log("isEnabled flag is ", isEnabled);
+
+printName(FirstName, LastName);
+
+
+function printName(FirstName:string, LastName:string) {
+    let FullName = FirstName + LastName;
+    
+    console.log( `My name is: ${FirstName} ${LastName} ` );
+    
+}
+
+
+//primitive types : object
+
+let course = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamental",
+    lessonsCount: 10
+};
+
+console.log("type of course is", typeof course)
+
+
+// Defining inline annotation type of an object 
+//not a good practice it's just to undestand how types can be defined for object properties
+
+let course1: {
+    title: string,
+    subtitle: string,
+    lessonsCount: number,
+    author:{
+        firstName: string,
+        lastName: string
+    }
+} = {
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language fundamental",
+    lessonsCount: 10,
+    author: {
+        firstName: 'Shivam',
+        lastName: 'Patel'
+    }
+};
+
+console.log("type of course is", typeof course1);
--- fundamentals/dist/01-why-typescript.js
+++ fundamentals/dist/01-why-typescript.js
@@ -1,7 +1,8 @@
-var courseName = "TypeScript Bootcamp V5";
+"use strict";
+let courseName = "TypeScript Bootcamp V5";
 courseName = "Hello";
 printCourse(courseName);
 function printCourse(name) {
-    "";
+    ``;
     console.log(name.toUpperCase());
 }
