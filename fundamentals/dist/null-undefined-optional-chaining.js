8c1fad57224b5cbf32cc13d9270101ed4fb402a4
Shivam Patel <shivam.patel@tatatechnologies.com>
2023-12-11 11:16:59 +0530

    null undefined and optional chaining

---
+++ fundamentals/dist/null-undefined-optional-chaining.js
@@ -0,0 +1,14 @@
+"use strict";
+//undefined means that JS variable has yet not been initialized, it's just defined.
+//At the time of defination of any var JS assign a defualt values. ie. undefined
+let title;
+console.log("title = " + title);
+if (!title) {
+    console.log("THe value of title not known yet!");
+}
+//null means that the variable is not holding any value currently but it can be assigned with any value and can be re assigned with null again
+let title2 = null;
+console.log("title = " + title2);
+if (!title2) {
+    console.log(`The value of title is ${title2}!`);
+}
